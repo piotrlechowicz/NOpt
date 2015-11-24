5b68c1c125a5737240203124feaf7f0b036267ab
piotrlechowicz <piotr.tobiasz.lechowicz@gmail.com>
2015-11-25 00:30:03 +0100

    Connect gui with logic

--- NewtonOpt.py
+++ NewtonOpt.py
@@ -23,25 +23,124 @@ from optimization.newton import NewtonAlgorithm
 from numpy import array
 from qtgui import gui
 
+from application.ApplicationProperties import NewtonAlgorithmProperties, DrawingProperties
+
 # Interesting function: (x+1)^2 * (y-1)^4 + (y+1)^4 * (x-1)^2
 
-import matplotlib
-# matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
 
 class App(QMainWindow, gui.Ui_MainWindow):
+    """Main class which controls the flow of an application"""
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi(self)                                          # create gui components
+        self.newton_properties = NewtonAlgorithmProperties()        # create properties holder
+        self.drawing_properties = DrawingProperties()               # drawing properties holder
+        self.__set_fields_logic()                                   # connect gui fields to the logic of an application
+        self.update_fields()                                        # get values of all fields
 
-        # define connections
-        self.executeButton.clicked.connect(self.executeButtonClicked)
+    def __set_fields_logic(self):
+        """Set connection between fields and its logic"""
+        self.executeButton.clicked.connect(self.execute_button_clicked)
+        self.npStartingPointX.textChanged.connect(self.set_np_starting_point)
+        self.npStartingPointY.textChanged.connect(self.set_np_starting_point)
+        self.npStartBoundaryX.textChanged.connect(self.set_np_start_boundary)
+        self.npStartBoundaryY.textChanged.connect(self.set_np_start_boundary)
+        self.npStopBoundaryX.textChanged.connect(self.set_np_stop_boundary)
+        self.npStopBoundaryY.textChanged.connect(self.set_np_stop_boundary)
+        self.npResolutionX.textChanged.connect(self.set_np_resolution)
+        self.npResolutionY.textChanged.connect(self.set_np_resolution)
+        self.npTolerance.textChanged.connect(self.set_np_tolerance)
+        self.dpStartBoundaryX.textChanged.connect(self.set_dp_start_boundary)
+        self.dpStartBoundaryY.textChanged.connect(self.set_dp_start_boundary)
+        self.dpStopBoundaryX.textChanged.connect(self.set_dp_stop_boundary)
+        self.dpStopBoundaryY.textChanged.connect(self.set_dp_stop_boundary)
+        self.dpResolutionX.textChanged.connect(self.set_dp_resolution)
+        self.dpResolutionY.textChanged.connect(self.set_dp_resolution)
 
-    def executeButtonClicked(self):
-        print "exec clicked"
+    def update_fields(self):
+        """Update values of all fields"""
+        np = self.newton_properties.get_all_properties()
+        dp = self.drawing_properties.get_all_properties()
+        self.npStartingPointX.setText(str(np[0][0]))
+        self.npStartingPointY.setText(str(np[0][1]))
+        self.npStartBoundaryX.setText(str(np[1][0]))
+        self.npStartBoundaryY.setText(str(np[1][1]))
+        self.npStopBoundaryX.setText(str(np[2][0]))
+        self.npStopBoundaryY.setText(str(np[2][1]))
+        self.npResolutionX.setText(str(np[3][0]))
+        self.npResolutionY.setText(str(np[3][1]))
+        self.npTolerance.setText(str(np[4]))
+        self.dpStartBoundaryX.setText(str(dp[0][0]))
+        self.dpStartBoundaryY.setText(str(dp[0][1]))
+        self.dpStopBoundaryX.setText(str(dp[1][0]))
+        self.dpStopBoundaryY.setText(str(dp[1][1]))
+        self.dpResolutionX.setText(str(dp[2][0]))
+        self.dpResolutionY.setText(str(dp[2][1]))
+
+    def execute_button_clicked(self):
+        # todo: implement this
+        print self.newton_properties.get_all_properties()
+        print self.drawing_properties.get_all_properties()
+        pass
+
+    def set_np_starting_point(self):
+        self.__text_filed_caller(self.newton_properties.set_starting_point,
+                                 self.npStartingPointX,
+                                 self.npStartingPointY)
+
+    def set_np_start_boundary(self):
+        self.__text_filed_caller(self.newton_properties.set_start_boundary,
+                                 self.npStartBoundaryX,
+                                 self.npStartBoundaryY)
+
+    def set_np_stop_boundary(self):
+        self.__text_filed_caller(self.newton_properties.set_stop_boundary,
+                                 self.npStopBoundaryX,
+                                 self.npStopBoundaryY)
+
+    def set_np_resolution(self):
+        self.__text_filed_caller(self.newton_properties.set_resolution,
+                                 self.npResolutionX,
+                                 self.npResolutionY)
+
+    def set_np_tolerance(self):
+        self.__text_filed_caller(self.newton_properties.set_tolerance,
+                                 self.npTolerance)
+
+    def set_dp_start_boundary(self):
+        self.__text_filed_caller(self.drawing_properties.set_start_boundary,
+                                 self.dpStartBoundaryX,
+                                 self.dpStartBoundaryY)
+
+    def set_dp_stop_boundary(self):
+        self.__text_filed_caller(self.drawing_properties.set_stop_boundary,
+                                 self.dpStopBoundaryX,
+                                 self.dpStopBoundaryY)
+
+    def set_dp_resolution(self):
+        self.__text_filed_caller(self.drawing_properties.set_resolution,
+                                 self.dpResolutionX,
+                                 self.dpResolutionY)
+
+    @staticmethod
+    def __text_filed_caller(function, *fields):
+        """function used to holding errors from getting values of text fields
+        It colours incorrect output with red and disable the possibility
+        of calling algorithm when error occurred.
+        """
+        params = []
+        for field in fields:
+            params.append(field.text())
+        if function(*params):
+            for field in fields:
+                field.setStyleSheet("background-color: white")
+        else:
+            for field in fields:
+                field.setStyleSheet("background-color: #aa0000")
 
     def run(self):
         # main program loop
@@ -85,6 +184,7 @@ class App(QMainWindow, gui.Ui_MainWindow):
             plotter = Plotter(goal_function.expression, goal_function.function_name, *parameters_drawing)
             plotter.plot_function_in_3D()
 
+            # todo: move it to plotter
             # add plot on the gui
             figure = plt.figure()
             canvas = FigureCanvas(figure)
@@ -128,5 +228,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     form = App()
     form.show()
-    form.run()
+
     app.exec_()
+    form.run()
---
+++ application/ApplicationProperties.py
@@ -0,0 +1,95 @@
+class Properties:
+    """Abstract class of necessary properties"""
+    def __init__(self):
+        self.start_boundary = [-5., -5.]
+        self.stop_boundary = [5., 5.]
+        self.resolution = [0.1, 0.1]
+
+    def set_default_values(self):
+        self.start_boundary = [-5., -5.]
+        self.stop_boundary = [5., 5.]
+        self.resolution = [0.1, 0.1]
+
+    def set_start_boundary(self, x, y):
+        try:
+            x = float(x)
+            y = float(y)
+            self.start_boundary = [x, y]
+            return True
+        except ValueError:
+            return False
+
+    def set_stop_boundary(self, x, y):
+        try:
+            x = float(x)
+            y = float(y)
+            self.stop_boundary = [x, y]
+            return True
+        except ValueError:
+            return False
+
+    def set_resolution(self, x, y):
+        try:
+            x = float(x)
+            y = float(y)
+            self.resolution = [x, y]
+            return True
+        except ValueError:
+            return False
+
+    def get_all_properties(self):
+        return [self.start_boundary, self.stop_boundary, self.resolution]
+
+
+class NewtonAlgorithmProperties(Properties):
+    def __init__(self):
+        Properties.__init__(self)
+        self.starting_point = [1., 1.]
+        self.tolerance = 0.1
+
+    def __set_default_values(self):
+        Properties.set_default_values(self)
+        self.starting_point = [1., 1.]
+        self.tolerance = 0.1
+
+    def set_starting_point(self, x, y):
+        try:
+            x = float(x)
+            y = float(y)
+            self.starting_point = [x, y]
+            return True
+        except ValueError:
+            return False
+
+    def set_tolerance(self, tolerance):
+        try:
+            tolerance = float(tolerance)
+            self.tolerance = tolerance
+            return True
+        except ValueError:
+            return False
+
+    def get_all_properties(self):
+        properties = Properties.get_all_properties(self)
+        properties.insert(0, self.starting_point)
+        properties.append(self.tolerance)
+        return properties
+
+
+class DrawingProperties(Properties):
+    def __init__(self):
+        Properties.__init__(self)
+        self.max_nr_of_points = 1000
+
+    def set_resolution(self, x, y):
+        try:
+            x = float(x)
+            y = float(y)
+            if (self.stop_boundary[0] - self.start_boundary[0]) / x > self.max_nr_of_points:
+                x = self.stop_boundary[0] - self.start_boundary[0] / self.max_nr_of_points
+            if (self.stop_boundary[1] - self.start_boundary[1]) / x > self.max_nr_of_points:
+                y = self.stop_boundary[1] - self.start_boundary[1] / self.max_nr_of_points
+            self.resolution = [x, y]
+            return True
+        except ValueError:
+            return False
